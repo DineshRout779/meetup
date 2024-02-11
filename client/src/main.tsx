22e2ffa0543f3b84f1f21a153344e89a1f193bd4
Dinesh Rout <rddinesh779@gmail.com>
2024-02-12 00:33:07 +0530

    feat: routing

---
+++ client/src/main.tsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Room from './pages/Room.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/room/:roomId',
+    element: <Room />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+
+  // </React.StrictMode>,
+);
